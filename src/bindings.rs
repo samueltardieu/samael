e43705d776c436fc1eb8e9412ffb6ae0d59603a6
Samuel Tardieu <sam@rfc1149.net>
2022-02-09 00:15:05 +0100

    Limit bindings to xmlSec specific entities

--- src/bindings.rs
+++ src/bindings.rs
@@ -6,6 +6,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 #![allow(improper_ctypes)]
+#![allow(deref_nullptr)]
 #![allow(clippy::upper_case_acronyms)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
